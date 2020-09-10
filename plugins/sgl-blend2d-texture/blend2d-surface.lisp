ecf46923329ca7738c902e75d7f72a1643b8a302
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2020-09-09 19:47:51 -0600

    Initial commit
    Not working yet...

---
+++ plugins/sgl-blend2d-texture/blend2d-surface.lisp
@@ -0,0 +1,43 @@
+;; blend2d-surface.lisp
+;;
+;; Copyright (c) 2020 Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
+
+
+;; Permission to use, copy, modify, and/or distribute this software for any
+;; purpose with or without fee is hereby granted, provided that the above
+;; copyright notice and this permission notice appear in all copies.
+
+;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+(in-package :blend2d-surface)
+
+(defclass vertex-object (vertex-object)
+  ((vertices :initarg :vertices)
+   (indices :initarg :indices)
+   (primitive-type :initarg :primitive-type :initform :triangles))
+  (:documentation "Base class for all objects that can be rendered in a scene."))
+
+(defmethod fill-buffers ((object vertex-object))
+  (with-slots (vbos ebos vertices indices) object
+    (cond ((null vbos)
+           (setf vbos (gl:gen-buffers 1))
+           (setf ebos (gl:gen-buffers 1))
+           (let ((gl-vertices (to-gl-float-array vertices))
+                 (gl-indices (to-gl-array indices :unsigned-int)))
+
+             (gl:bind-buffer :array-buffer (car vbos))
+             (gl:buffer-data :array-buffer :dynamic-draw gl-vertices)
+             (gl:free-gl-array gl-vertices)
+
+             (gl:bind-buffer :element-array-buffer (car ebos))
+             (gl:buffer-data :element-array-buffer :dynamic-draw gl-indices)
+             (gl:free-gl-array gl-indices)))
+          (t
+           (gl:bind-buffer :array-buffer (car vbos))
+           (gl:bind-buffer :element-array-buffer (car ebos))))))
