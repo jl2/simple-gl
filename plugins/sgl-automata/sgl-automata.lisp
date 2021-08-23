a86aaa76facfcb1940e2e1458f9f02b998f8fb48
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2021-08-23 11:03:12 -0600

    A little code cleanup, add some comments

--- plugins/sgl-automata/sgl-automata.lisp
+++ plugins/sgl-automata/sgl-automata.lisp
@@ -30,14 +30,15 @@
    (next-row-data :initform nil :initarg :next-row-data :type (or null (SIMPLE-ARRAY BIT (*))))
    ))
 
-(defun create-cellular-automata (width &key
-                                         (rule 90)
-                                         (max-instances (* width 1000))
-                                         (initial-data))
+(defun create-cellular-automata (width
+                                 &key
+                                   (rule 90)
+                                   (max-instances (* width 1000))
+                                   (initial-data))
   "Create a cellular automata of the specified size."
   (let* ((init-data (if (null initial-data)
-                                   (make-array (list width) :initial-element 0 :element-type 'bit)
-                                   (coerce initial-data '(simple-array bit (*)))))
+                        (make-array (list width) :initial-element 0 :element-type 'bit)
+                        (coerce initial-data '(simple-array bit (*)))))
          (ret-val (make-instance 'cellular-automata
                                  :max-instances max-instances
                                  :width width
@@ -52,30 +53,43 @@
 
 (defun add-row-instances (object)
   "Draw the next row of automata data by adding translations to the instance buffer."
+
   (with-slots (buffers instance-count max-instances width current-row current-row-data) object
     (let ((buffer (get-buffer object :obj-transform))
           (cell-size (/ 2.0 width)))
       (with-slots (pointer) buffer
-        (loop with updated = nil
-              for x-offset from 0
-              for x-float = (- 1.0 (* cell-size x-offset))
-              for rv across current-row-data
-              for y-offset = current-row
-              for y-float = (1- (* 2 (/ (1- y-offset) width)))
-              until (>= instance-count max-instances)
-              when (= 1 rv) do
+        ;; Loop over each cell in the data
+        (loop
+          ;; Track whether any quads have been added
+          with updated = nil
+
+          ;; Calculate the quad location
+          for x-offset fixnum from 0
+          for x-float real = (- 1.0 (* cell-size x-offset))
+          for y-offset fixnum = current-row
+          for y-float real = (1- (* 2 (/ (1- y-offset) width)))
+
+          ;; Bail out when there's no room for a new quad.
+          until (>= instance-count max-instances)
+
+          ;; If the cell is 'on' then add a quad
+          for rv bit across current-row-data
+          when (= 1 rv) do
                 (sgl:fill-pointer-offset (vec3 x-float y-float 0.0)
                                          pointer
                                          (* instance-count 3))
                 (setf updated t)
                 (incf instance-count)
-              finally
-                 (when updated
-                   (reload buffer)))))))
 
-(declaim (inline apply-rule))
+          finally
+             ;; Copy the buffer to OpenGL if anything changed.
+             (when updated
+               (reload buffer)))))))
+
+(declaim (inline apply-rule left-element right-element compute-next-row add-row-instance))
 
 (defun apply-rule (rule lb cb rb)
+  "Compute output cell value based on left, center, and right cells."
   (declare (type bit lb cb rb)
            (type fixnum rule))
   (let ((idx (+ (* 4 lb) (* 2 cb) rb)))
@@ -83,8 +97,12 @@
         1
         0)))
 
-(defgeneric compute-left-idx (ca idx))
-(defgeneric compute-right-idx (ca idx))
+;; Override these to simulate different board types (wrap around, etc.)
+(defgeneric left-element (ca idx)
+  (:documentation "Look up the cell value 'left' of idx."))
+
+(defgeneric right-element (ca idx)
+  (:documentation "Look up the cell value 'right' of idx."))
 
 (defmethod left-element ((ca cellular-automata) idx)
   (with-slots (current-row-data width) ca
@@ -93,25 +111,27 @@
                         (1- idx))))
       (aref current-row-data real-idx))))
 
-(defmethod right-element (ca idx)
+;; This implementation wraps around to the other side of the board
+(defmethod right-element ((ca cellular-automata) idx)
   (with-slots (current-row-data width) ca
     (let ((real-idx (if (= idx (1- width))
                         0
                         (1+ idx))))
       (aref current-row-data real-idx))))
 
-(defun compute-next-row (object)
-  (declare (ignorable object))
+(defgeneric compute-next-row (object)
+  (:documentation "Compute the next row of the automata."))
+
+(defmethod compute-next-row ((object cellular-automata))
   (with-slots (instance-count rule max-instances width current-row current-row-data next-row-data) object
-    (loop for idx from 0
-          for left-bit = (left-element object idx)
-          for cur-bit across current-row-data
-          for right-bit = (right-element object idx)
+    (loop for idx fixnum from 0
+          for left-bit bit = (left-element object idx)
+          for cur-bit bit across current-row-data
+          for right-bit bit = (right-element object idx)
           do
              (setf (aref next-row-data idx) (apply-rule rule left-bit cur-bit right-bit)))
     (rotatef current-row-data next-row-data)
     (incf current-row)))
-
 
 (defmethod update ((object cellular-automata) elapsed-seconds)
   (declare (ignorable elapsed-seconds))
@@ -123,8 +143,10 @@
 (defmethod initialize-buffers ((object cellular-automata) &key)
   (when (buffers object)
     (error "Object buffers already setup!"))
+
+  ;; Fill vertex and index buffers with data for a single OpenGL quad
   (with-slots (width) object
-    (let ((fw (/ 2.0f0 width)))
+    (let ((cell-width (/ 2.0f0 width)))
       (set-buffer object
                   :vertices
                   (make-instance
@@ -134,13 +156,13 @@
                              28
                              (list 0.0f0 0.0f0 0.0f0
                                0.1f0 0.8f0 0.1f0 1.0f0
-                               fw 0.0f0 0.0f0
+                               cell-width 0.0f0 0.0f0
                                0.1f0 0.8f0 0.1f0 1.0f0
 
-                               fw fw 0.0f0
+                               cell-width cell-width 0.0f0
                                0.1f0 0.8f0 0.1f0 1.0f0
 
-                               0.0f0 fw 0.0f0
+                               0.0f0 cell-width 0.0f0
                                0.1f0 0.8f0 0.1f0 1.0f0))
                    :stride nil
                    :attributes '(("in_position" . :vec3)
@@ -156,11 +178,14 @@
                :stride nil
                :usage :static-draw
                :free t))
+
+  ;; Create an empty instance buffer that will be filled with vec3 translations
   (with-slots (max-instances instance-count) object
     (setf instance-count 0)
     (set-buffer object
                 :obj-transform (make-instance
                                 'instance-buffer
+                                ;; Not technically empty, but this gets overwritten immediately
                                 :pointer (to-gl-array :float
                                                       (* max-instances 3)
                                                       (vec3 0.0 0.0 0.0))
